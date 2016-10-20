1006524062dde4ec7383a38bfae4747c9210fe6a
Preston Exzabe <pexzabe@hawaii.edu>
2016-10-19 19:09:24 -1000

    created contact pages

---
+++ app/imports/ui/pages/add-contact-page.js
@@ -0,0 +1,7 @@
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+import { Stuff } from '../../api/stuff/stuff.js';
+
+/* eslint-disable object-shorthand, no-unused-vars */
+
